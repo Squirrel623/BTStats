1f73e7498883d2491bb2b02903da8520c2b5394a
Brady Hutchins <squirrel623@yahoo.com>
2018-01-08 18:25:47 -0500

    Remove unnecessary package

--- BTStatsCore/wwwsrc/src/index.js
+++ BTStatsCore/wwwsrc/src/index.js
@@ -8,18 +8,14 @@ import popover from 'angular-ui-bootstrap/src/popover';
 import carousel from 'angular-ui-bootstrap/src/carousel';
 import button from 'angular-ui-bootstrap/src/buttons';
 
-import calendar from 'angular-calendar-heatmap';
-
 import btStatsComponent,{name as btStatsComponentName} from './bt-stats.component';
 import heatmapMonth, {name as heatmapMonthDirectiveName} from './my-heatmap/month/month.directive';
 import userDataStore, {name as userDataStoreServiceName} from './user-data-store';
-//import calendarDirective, {name as calendarDirectiveName} from './heatmap/calendar-heatmap.directive';
 
-const btStatsModule = angular.module('btStats', [angularAnimate, angularTouch, typeahead, calendar, popover, carousel, button]);
+const btStatsModule = angular.module('btStats', [angularAnimate, angularTouch, typeahead, popover, carousel, button]);
 
 btStatsModule.component(btStatsComponentName, btStatsComponent);
 btStatsModule.directive(heatmapMonthDirectiveName, heatmapMonth);
 btStatsModule.service(userDataStoreServiceName, userDataStore);
-//btStatsModule.directive(calendarDirectiveName, calendarDirective);
 
 export default btStatsModule;
